0b544f1e681f0b4e77013e09341ba776b4b78607
Oliver Cook <olliewatford@gmail.com>
2024-02-25 20:16:44 +0000

    fixed issue with image on mobile

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -3,8 +3,8 @@ import Link from '@/app/components/Link'
 
 export default function Image() {
   return (
-    <div className="relative w-5/6 h-fit lg:h-5/6 lg:w-fit  border-4 border-cyan-400 box-content rounded-[17px]">
-      <img src="/apps.jpg" alt="A dog" className="h-full rounded-xl" />
+    <div className="relative w-5/6 lg:h-5/6 lg:w-fit  border-4 border-cyan-400 box-content rounded-[17px]">
+      <img src="/apps.jpg" alt="A dog" className="lg:h-full rounded-xl object-contain" />
       {sections.map((section, index) => {
         const guid=crypto.randomUUID()
 
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { IoIosArrowDown } from "react-icons/io";
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <div className="h-screen w-full relative flex flex-col items-center justify-center">
+      <div className="h-[100svh] w-full relative flex flex-col items-center justify-center">
         <p className="text-center font-semibold mt-12 text-sm mb-2">I created 30 web apps in 30 days. Click on the squares below to see them.</p>
         <Image />
         <p className="absolute bottom-4 right-4">Scroll to read more<IoIosArrowDown className="inline h-6 w-6" /></p>
